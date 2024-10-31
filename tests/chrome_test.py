6cc03f6f3710bb06861a85f7b93cd3d3fa44661e
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2024-10-31 15:46:47 +0530

    Update

--- tests/chrome_test.py
+++ tests/chrome_test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 options.add_argument("--headless=new")
-chrome_options.add_argument('--no-sandbox')
+options.add_argument('--no-sandbox')
 # options.add_argument("--disable-gpu")
 # options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
