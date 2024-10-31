e9c86538c659c3e103968fe7d97ec6f9ad351ff2
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2024-10-31 15:38:03 +0530

    Add disable gpu

--- tests/chrome_test.py
+++ tests/chrome_test.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 options.add_argument("--headless=new")
+options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=options)
 driver.get('http://selenium.dev/')
 print('Title', driver.title)
